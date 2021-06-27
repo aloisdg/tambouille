7e9bf488599370606393dcbbd73a4ccaaaf41b11
aloisdg <aloisdegouvello@live.fr>
2021-06-27 16:19:38 +0200

    Add wikipedia as figure

---
+++ components/Wikipedia.js
@@ -0,0 +1,8 @@
+import Figure from './Figure'
+
+const Wikipedia = (wikipediaProps) => {
+  let props = { ...wikipediaProps, host: 'Wikipedia', hosturl: 'https://fr.wikipedia.org' }
+  return <Figure {...props} />
+}
+
+export default Wikipedia
